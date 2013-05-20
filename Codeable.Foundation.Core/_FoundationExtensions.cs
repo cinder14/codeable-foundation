26aa4ca683ba3c8d72faf1f4a4f52c2ec70edcee
William Mansfield <wmansfield@codeable.net>
2013-05-20 10:00:07 -0700

    extensions

--- Codeable.Foundation.Core/_FoundationExtensions.cs
+++ Codeable.Foundation.Core/_FoundationExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.Practices.Unity;
 using Codeable.Foundation.Common;
 using Codeable.Foundation.Core;
 using Codeable.Foundation.Common.System;
+using Codeable.Foundation.Core.Caching;
 
 namespace Codeable.Foundation.Common
 {
@@ -19,17 +20,28 @@ namespace Codeable.Foundation.Common
         {
             return iFoundation.Container.Resolve<T>(name, overrides);
         }
-        public static void LogError<T>(this IFoundation iFoundation, Exception ex, string tag = "")
+        public static void LogError(this IFoundation iFoundation, Exception ex, string tag = "")
         {
             iFoundation.GetLogger().Write(CoreUtility.FormatException(ex, tag), Category.Error);
         }
-        public static void LogError<T>(this IFoundation iFoundation, string message, string tag = "")
+        public static void LogError(this IFoundation iFoundation, string message, string tag = "")
         {
             iFoundation.GetLogger().Write(message, Category.Error);
         }
-        public static void LogWarning<T>(this IFoundation iFoundation, string warning)
+        public static void LogWarning(this IFoundation iFoundation, string message)
         {
-            iFoundation.GetLogger().Write(warning, Category.Warning);
+            iFoundation.GetLogger().Write(message, Category.Warning);
+        }
+        public static void LogTrace(this IFoundation iFoundation, string message)
+        {
+            iFoundation.GetLogger().Write(message, Category.Trace);
+        }
+
+        public static T CachePerFoundation<T>(this IFoundation iFoundation, string callerName, Func<T> retrieveMethod)
+        {
+            // works because we know internals here, should create instance, and use it, but.. performance is better this way
+            AspectCache cache = iFoundation.Resolve<AspectCache>();
+            return cache.PerFoundation(callerName, retrieveMethod);
         }
     }
 }
