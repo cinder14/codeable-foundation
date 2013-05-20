cb95eccb99096b4cff0c9053de66c19e26be4e31
William Mansfield <wmansfield@codeable.net>
2013-05-20 15:32:35 -0700

    apicontroller support

---
+++ Codeable.Foundation.Core/Unity/ExpireStaticLifetimeManager.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+using System.Web;
+using System.Diagnostics;
+using Codeable.Foundation.Core.System;
+using Codeable.Foundation.Common.System;
+
+namespace Codeable.Foundation.Core.Unity
+{
+    public class ExpireStaticLifetimeManager : LifetimeManager
+    {
+        public ExpireStaticLifetimeManager(string globalKey, TimeSpan lifeSpan, bool renewOnAccess = false)
+        {
+            this.GlobalKey = "ExpireStaticLifetimeManager" + globalKey;
+            this.LifeSpan = lifeSpan;
+            this.RenewOnAccess = renewOnAccess;
+        }
+        ~ExpireStaticLifetimeManager()
+        {
+            this.Dispose(false);
+        }
+        protected string GlobalKey { get; set; }
+        protected TimeSpan LifeSpan { get; set; }
+        protected bool RenewOnAccess { get; set; }
+        protected static object _creationLock = new object();
+        protected static object _accessLock = new object();
+        protected Dictionary<string, ExpireStaticValue> StaticItems
+        {
+            get
+            {
+                if (Single<Dictionary<string, ExpireStaticValue>>.Instance == null)
+                {
+                    lock (_creationLock)
+                    {
+                        if (Single<Dictionary<string, ExpireStaticValue>>.Instance == null)
+                        {
+                            Single<Dictionary<string, ExpireStaticValue>>.Instance = new Dictionary<string, ExpireStaticValue>(StringComparer.OrdinalIgnoreCase);
+                        }
+                    }
+                }
+                return Single<Dictionary<string, ExpireStaticValue>>.Instance;
+            }
+        }
+
+        public override object GetValue()
+        {
+            object result = null;
+            lock (_accessLock)
+            {
+                if (this.StaticItems.ContainsKey(this.GlobalKey))
+                {
+                    ExpireStaticValue value = this.StaticItems[GlobalKey];
+                    if (value != null)
+                    {
+                        if(value.AllowAccess())
+                        {
+                            result = value.Value;
+                        }
+                        else
+                        {
+                            value.Dispose();
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        public override void RemoveValue()
+        {
+            lock (_accessLock)
+            {
+
+                if (this.StaticItems.ContainsKey(this.GlobalKey))
+                {
+                    ExpireStaticValue value = this.StaticItems[this.GlobalKey];
+                    this.StaticItems.Remove(this.GlobalKey);
+                    if (value != null)
+                    {
+                        value.Dispose();
+                    }
+                }
+            }
+        }
+        public override void SetValue(object newValue)
+        {
+            this.RemoveValue();
+            lock (_accessLock)
+            {
+                this.StaticItems[this.GlobalKey] = new ExpireStaticValue(this.RenewOnAccess, this.LifeSpan, newValue);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool disposing)
+        {
+
+        }
+
+
+
+
+        public class ExpireStaticValue : IDisposable
+        {
+            public ExpireStaticValue(bool renewOnAccess, TimeSpan lifeSpan, object value)
+            {
+                this.LifeSpan = lifeSpan;
+                this.RenewOnAccess = renewOnAccess;
+                this.Value = value;
+                this.RenewLease();
+            }
+            ~ExpireStaticValue()
+            {
+                this.Dispose(false);
+            }
+
+            public bool RenewOnAccess;
+            public TimeSpan LifeSpan;
+            public object Value;
+            public DateTime UtcExpire;
+
+            public void RenewLease()
+            {
+                UtcExpire = DateTime.UtcNow.Add(this.LifeSpan);
+            }
+            public bool AllowAccess()
+            {
+                if (UtcExpire < DateTime.UtcNow)
+                {
+                    return false;
+                }
+                if (this.RenewOnAccess)
+                {
+                    this.RenewLease();
+                }
+                return true;
+            }
+
+            public void Dispose()
+            {
+                this.Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            protected void Dispose(bool disposing)
+            {
+                try
+                {
+                    if (disposing)
+                    {
+                        object value = this.Value;
+                        this.Value = null;
+                        if (value != null)
+                        {
+                            IDisposable disposable = value as IDisposable;
+                            if (disposable != null)
+                            {
+                                disposable.Dispose();
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+
+            
+        }
+
+
+    }
+}
---
+++ Codeable.Foundation.Web.Core/MVC/CoreApiController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Codeable.Foundation.Core;
+using Codeable.Foundation.Common;
+using Codeable.Foundation.Common.System;
+using Microsoft.Practices.Unity;
+using System.Diagnostics;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Codeable.Foundation.UI.Web.Core.MVC
+{
+    [DebuggerStepThrough]
+    public class CoreApiController : ApiController
+    {
+        public CoreApiController()
+            : this(CoreFoundation.Current)
+        {
+        }
+        public CoreApiController(IFoundation iFoundation)
+            : this(iFoundation, null)
+        {
+        }
+        public CoreApiController(IFoundation iFoundation, IHandleExceptionProvider iHandleExceptionProvider)
+        {
+            this.IFoundation = iFoundation;
+            this.IHandleExceptionProvider = iHandleExceptionProvider;
+            if (this.IHandleExceptionProvider == null)
+            {
+                this.IHandleExceptionProvider = this.IFoundation.Container.Resolve<IHandleExceptionProvider>();
+                this.IHandleExceptionProvider.PolicyName = this.GetType().ToString();
+            }
+
+        }
+
+        protected virtual void Construct()
+        {
+            // virtual constructor
+        }
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+
+            Construct();
+        }
+        protected virtual IFoundation IFoundation { get; set; }
+        protected virtual IHandleExceptionProvider IHandleExceptionProvider { get; set; }
+
+        protected virtual T ExecuteFunction<T>(string methodName, Func<T> function, params object[] parameters)
+        {
+            return IFoundation.GetAspectCoordinator().WrapFunctionCall<T>(this, methodName, parameters, false, this.IHandleExceptionProvider, function);
+        }
+        protected virtual T ExecuteFunction<T>(string methodName, bool forceThrow, Func<T> function, params object[] parameters)
+        {
+            return IFoundation.GetAspectCoordinator().WrapFunctionCall<T>(this, methodName, parameters, forceThrow, this.IHandleExceptionProvider, function);
+        }
+        protected virtual void ExecuteMethod(string methodName, Action action, params object[] parameters)
+        {
+            this.IFoundation.GetAspectCoordinator().WrapMethodCall(this, methodName, parameters, false, this.IHandleExceptionProvider, action);
+        }
+        protected virtual void ExecuteMethod(string methodName, bool forceThrow, Action action, params object[] parameters)
+        {
+            this.IFoundation.GetAspectCoordinator().WrapMethodCall(this, methodName, parameters, forceThrow, this.IHandleExceptionProvider, action);
+        }
+
+    }
+}
