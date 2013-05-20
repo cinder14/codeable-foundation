4d8e40ae6c20261230bd018d862a66af2cd3deee
William Mansfield <wmansfield@codeable.net>
2013-05-20 09:26:14 -0700

    Added error logging extension methods

--- Codeable.Foundation.Core/_FoundationExtensions.cs
+++ Codeable.Foundation.Core/_FoundationExtensions.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Microsoft.Practices.Unity;
 using Codeable.Foundation.Common;
+using Codeable.Foundation.Core;
+using Codeable.Foundation.Common.System;
 
 namespace Codeable.Foundation.Common
 {
@@ -16,6 +18,18 @@ namespace Codeable.Foundation.Common
         public static T Resolve<T>(this IFoundation iFoundation, string name, params ResolverOverride[] overrides)
         {
             return iFoundation.Container.Resolve<T>(name, overrides);
+        }
+        public static void LogError<T>(this IFoundation iFoundation, Exception ex, string tag = "")
+        {
+            iFoundation.GetLogger().Write(CoreUtility.FormatException(ex, tag), Category.Error);
+        }
+        public static void LogError<T>(this IFoundation iFoundation, string message, string tag = "")
+        {
+            iFoundation.GetLogger().Write(message, Category.Error);
+        }
+        public static void LogWarning<T>(this IFoundation iFoundation, string warning)
+        {
+            iFoundation.GetLogger().Write(warning, Category.Warning);
         }
     }
 }
